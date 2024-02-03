4231a40cda072fdc021d5908926c069bcee7e20a
Alexander Shadrin <144260991+Sh-Shanya@users.noreply.github.com>
2024-02-03 17:31:04 +0300

    Add files via upload

---
+++ static.py
@@ -0,0 +1,21 @@
+import sys
+from io import BytesIO
+import requests
+from PIL import Image
+import geocoder
+
+
+def get_image(ll, spn, l):
+
+    map_params = {
+        "ll": ll,
+        "spn": spn,
+        "l": l,
+    }
+
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+
+    with open("test.png", "wb") as f:
+        f.write(response.content)
+
